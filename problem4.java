96f225b8ae4445f806ede115435da404a4647cc3
S <s>
2023-10-01 16:15:49 +0400

    {"types":"C","runs":1}

--- problem4.java
+++ problem4.java
@@ -1,4 +1,16 @@
-
-public class problem4 {
-
+import stanford.karel.SuperKarel;
+/*კარელი დგას 1x1 უჯრაზე, შეავსებინეთ მას პირველი ქუჩა ბრილიანტებით. ანუ პირველი
+ქუჩის ყველა უჯრაზე უნდა იდოს ზუსტად ერთი ბრილიანტი. ჩათვალეთ რომ საწყის
+სამყაროში ბრილიანტები არსად არ დევს.
+ *  
+ */
+public class problem4 extends SuperKarel {
+	public void run() {
+		while (frontIsClear()) {
+			putBeeper();
+			move();
+	
+		}
+	}
+	
 }
