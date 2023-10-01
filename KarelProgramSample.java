3693b706c9651cc569d45a678c0965e884331133
S <s>
2023-10-01 15:00:29 +0400

    {"types":"C","runs":5}

--- KarelProgramSample.java
+++ KarelProgramSample.java
@@ -8,10 +8,13 @@
  */
 
 import stanford.karel.*;
-
+/*
+ * კარელი დგას 1x1 უჯრაზე. 3x1 უჯრაზე დევს ბრილიანტი, კარელმა ბრილიანტი უნდა
+გადაიტანოს 4x1 უჯრაზე და დაამთავროს მუშაობა. 
+ */
 public class KarelProgramSample extends SuperKarel {
 	public void run() {
-		move();
+		
 	}
 	
 }
