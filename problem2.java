b79acf34b5c11eb9a8197854fba1589b2fef7fb9
S <s>
2023-10-01 15:07:24 +0400

    {"types":"C","runs":6}

--- problem2.java
+++ problem2.java
@@ -1,4 +1,12 @@
-
-public class problem2 {
-
+import stanford.karel.SuperKarel;
+/*კარელი დგას 1x1 უჯრაზე. 5x1 უჯრაზე დევს 10 ცალი ბრილიანტი, კარელმა ათივე
+ბრილიანტი უნდა გადაიტანოს 6x1 უჯრაზე.
+ * 
+ */
+public class problem2 extends SuperKarel {
+	public void run() {
+		for(int i = 0; i< 4; i++) {move();}
+		
+	}
+	
 }
