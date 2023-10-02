602b15e0db3035b060d4472c44a9d1d042df7e2a
S <s>
2023-10-02 21:44:08 +0400

    {"types":"C","runs":11}

--- problem5.java
+++ problem5.java
@@ -11,7 +11,6 @@ public class problem5 extends SuperKarel {
 	}
 		public void jump() {
 			accend();
-			turnRight();
 			move();
 			decend();
 		 }
@@ -20,6 +19,7 @@ public class problem5 extends SuperKarel {
 			while (rightIsBlocked()) {
 				move();
 			}
+			turnRight();
 	
 		}
 		public void decend() {
@@ -30,7 +30,6 @@ public class problem5 extends SuperKarel {
 			
 				
 		}
-		public void	
 		}
 			
 		
