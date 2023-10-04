f536514abd6636c66ae769f6a7ad1f7648f538a3
S <s>
2023-10-04 22:18:27 +0400

    {"types":"C","runs":12}

--- problem3.java
+++ problem3.java
@@ -1,13 +1,11 @@
 import stanford.karel.SuperKarel;
 /*
- *  კარელი დგას 1x1 უჯრაზე, გაიყვანეთ იგი პირველი ქუჩის ბოლომდე. გაითვალისწინეთ, რომ
-სამყაროს ზომა არ იცით. თქვენი პროგრამა უნდა მუშაობდეს ნებისმიერი ზომის
-სამყაროსათვის.
+ *  
  */
 public class problem3 extends SuperKarel {
 	public void run() {
-		while (frontIsClear()) {
-			move();
+		if (frontIsBlocked()) {
+			turnLeft();
 		}
 		
 	}
