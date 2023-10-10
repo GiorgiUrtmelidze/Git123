1098980a41d924abf577b80e27e921095b19d3dc
S <s>
2023-10-10 12:29:03 +0400

    {"types":"C","runs":1}

--- problem2.java
+++ problem2.java
@@ -1,41 +1,33 @@
 import stanford.karel.SuperKarel;
-/*კარელმა უნდა გამოაკლოს ორი რიცხვი, 2x1 უჯრაზე დევს n ცალი ბრილიანტი, 3x1 უჯრაზე
-m ცალი ბრილიანტი, კარელმა 4x1 უჯრაზე უნდა დადოს n-m ბრილიანტი. ჩათვალეთ, რომ
-კარელს ჩანთაში უსასრულო რაოდენობის ბრილიანტი აქვს და ჩათვალეთ, რომ n>=m.
+/*
  *  
  */
 public class problem2 extends SuperKarel {
 	public void run() {
-		move();
-		move();
-		turnRight();
-		turnRight();
-		while (beepersPresent()) {
-			pickBeeper();
+		goOut();
+		pickBeeper();
+		getInside();
+		
+	}
+	private void goOut() {
+		while (frontIsClear()) {
 			move();
-			pickBeeper();
-			turnRight();
-			turnRight();
-			move();
-			turnRight();
-			turnRight();
-			
 		}
+		turnRight();
+		move();
+		turnLeft();
+		move();
+	}
+	private void getInside() {
+		turnAround();
 		move();
 		turnRight();
-		turnRight();
-		while (beepersPresent()) {
-	         pickBeeper();
-	         move();
-	         move();
-	         putBeeper();
-	         turnLeft();
-	         turnLeft();
-	         move();
-	         move();
-	         turnLeft();
-	         turnLeft();
+		move();
+		turnLeft();
+		while (frontIsClear()) {
+			move();
 		}
+		turnAround();
 	}
 	
 }
