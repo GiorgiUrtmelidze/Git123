fb5bcb93c6fc8df039c7337a3aeefaf4ead003e0
S <s>
2023-10-10 13:17:52 +0400

    {"types":"C","runs":10}

--- problem5.java
+++ problem5.java
@@ -4,8 +4,17 @@ import stanford.karel.SuperKarel;
  */
 public class problem5 extends SuperKarel {
 	public void run() {
+		while (frontIsClear()) {
 		fillLine();
-		
+		goBack();
+		if (frontIsClear()) {
+		move();
+		turnRight();
+		move();
+		fillLine();
+		goBack();
+		}
+	}
 		
 		
 	}
@@ -16,12 +25,19 @@ public class problem5 extends SuperKarel {
 			if (frontIsClear()) {
 				move();
 			}
+		}
 			if (frontIsBlocked()) {
 				putBeeper();
 			}
-			
 		}
-	}
+	 
+		private void goBack() {
+		 turnAround();
+		 while (frontIsClear()) {
+			 move();
+		 }
+		 turnRight();
+	 }
 	
 }
 		
