08d4ce613d8672d0d68c2657b8462ec7259614d7
S <s>
2023-10-10 17:14:37 +0400

    {"types":"C","runs":0}

--- problem7.java
+++ problem7.java
@@ -5,15 +5,5 @@ import stanford.karel.SuperKarel;
 public class problem7 extends SuperKarel {
 	public void run() {
 		
-		while (frontIsClear()) {
-			if (beepersPresent()) {
-				move();
-			} else {
-					putBeeper();
-					move();
-				}
-			}
-		putBeeper();
 	}
-
-	}	
+}
