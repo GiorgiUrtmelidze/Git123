4648d19bb432b9e4a8a6c1259cd6dd97fb30b77d
S <s>
2023-10-10 17:28:19 +0400

    {"types":"C","runs":7}

--- Problem_7.java
+++ Problem_7.java
@@ -6,5 +6,7 @@ import acm.graphics.*;
 public class Problem_7 extends GraphicsProgram {
 	public void run() {
 		add(new GOval(100, 200), 100, 100);
+		GOval myOval = new GOval (100, 200);
+		add(myOval, 100 ,400);
 	}
 }
