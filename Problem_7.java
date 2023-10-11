cb7710367739e67b822592f8f684a64fee00aeba
S <s>
2023-10-11 22:48:52 +0400

    {"types":"C","runs":15}

--- Problem_7.java
+++ Problem_7.java
@@ -7,7 +7,8 @@ public class Problem_7 extends GraphicsProgram {
 	public void run() {
 		GRect rectangle = new GRect(500, 200);
 		add(rectangle, 400,400);
-		GLine line = new GLine (360, 300, 650, 300);
+		
+		GLine line = new GLine (360,300,650,300);
 		add(line);
 	}
 }
