199a65c344ed9d654e9bdf31a9fef8fe2e06c398
S <s>
2023-10-11 23:13:03 +0400

    {"types":"C","runs":29}

--- Problem_7.java
+++ Problem_7.java
@@ -23,7 +23,7 @@ public class Problem_7 extends GraphicsProgram {
 		GRect window2 = new GRect (50, 90);
 		add(window2, 770, 445);
 		
-		GLine line4 = new GLine (480,520,530,520);
+		GLine line4 = new GLine (480,470,530,470);
 		add(line4);
 		
 		
