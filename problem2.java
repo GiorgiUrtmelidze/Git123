c128f7d457399ec85ac369e1723d0acefc4a6fcb
S <s>
2023-10-15 20:56:32 +0400

    {"types":"C","runs":21}

--- problem2.java
+++ problem2.java
@@ -10,7 +10,7 @@ public class problem2 extends SuperKarel {
 		  move();		  
 	  }
 	  turnLeft();
-	  for (int i = 0; i <5; i++) {
+	  for (int i = 0; i <6; i++) {
 		  move();
 		  putBeeper();
 	  }
