61e2786044556afb6eca3c49f91c5973e017c9fa
S <s>
2023-10-15 21:32:24 +0400

    {"types":"C","runs":39}

--- problem16.java
+++ problem16.java
@@ -6,7 +6,7 @@ public class problem16 extends ConsoleProgram {
 	int first = readInt("Enter first number: ");
 	int second = readInt("Enter second number: ");
 	int third = readInt("Enter third number: ");
-	double total = first^2;
+	double total = ((first)*(second) + (first)*(third) + (second)*(third))/((first)*(second)*(third));
 	println("Your Number Is " + total + ".");
 	}
 
