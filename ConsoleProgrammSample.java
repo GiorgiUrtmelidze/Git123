808238392b177a84af07fe2874b5abadcf83375b
S <s>
2023-10-15 20:11:36 +0400

    {"types":"C","runs":0}

--- ConsoleProgrammSample.java
+++ ConsoleProgrammSample.java
@@ -1,24 +1,9 @@
-/*
- * File: FindRange.java
- * --------------------
- * This program is a stub for the FindRange problem, which finds the
- * smallest and largest values in a list of integers.
- */
-
 import acm.program.*;
 
 public class ConsoleProgrammSample extends ConsoleProgram {
 
 	public void run() {
-	int a = 5;
-	a = a + 5;
 	
-	if(2 == 2){
-		double b = 3;
-		b = 4;
-		b = a + 3;
-		
-	}
 	}
 
 }
