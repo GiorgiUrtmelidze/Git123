99fa489adbbd67233693f614739f5f2be42c2a78
S <s>
2023-10-15 20:41:40 +0400

    {"types":"C","runs":16}

--- problem15.java
+++ problem15.java
@@ -8,7 +8,7 @@ public class problem15 extends ConsoleProgram {
 		int numberSecond = readInt("Enter Second Number: ");
 		int total = ((numberOne + numberSecond)/2 );
 		double y = total + 0.5;
-		if (numberOne%2==0 && numberSecond%2==0) {
+		if ((numberOne + numberSecond)%2==0) {
 		println("The answer is ->" + total + "."); 
 		} else {
 			println("The answer is ->" + y + ".");
