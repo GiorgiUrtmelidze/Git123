13007f73e7566bcad62737102e88612613c47303
S <s>
2023-10-15 20:17:47 +0400

    {"types":"A","runs":1}

---
+++ problem15.java
@@ -0,0 +1,14 @@
+import acm.program.ConsoleProgram;
+
+public class problem15 extends ConsoleProgram {
+
+	public void run() {
+		println("Hello");
+		int numberOne = readInt("Enter First Number: ");
+		int numberSecond = readInt("Enter Second Number ");
+		int total = (numberOne + numberSecond)/2;
+		println(total);
+	
+	}
+
+}
