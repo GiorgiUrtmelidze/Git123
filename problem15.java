fb88facf9c4756a6d6f159aaef4adcbeb3cd12eb
S <s>
2023-10-15 20:18:27 +0400

    {"types":"C","runs":3}

--- problem15.java
+++ problem15.java
@@ -5,7 +5,7 @@ public class problem15 extends ConsoleProgram {
 	public void run() {
 		println("Hello");
 		int numberOne = readInt("Enter First Number: ");
-		int numberSecond = readInt("Enter Second Number; ");
+		int numberSecond = readInt("Enter Second Number: ");
 		int total = (numberOne + numberSecond)/2;
 		println(total);
 	
