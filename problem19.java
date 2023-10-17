70adb76df4871be05753633571c03f325b706cc9
S <s>
2023-10-17 16:38:54 +0400

    {"types":"C","runs":12}

--- problem19.java
+++ problem19.java
@@ -5,7 +5,7 @@ public class problem19 extends ConsoleProgram {
 	public void run() {
 		int n = readInt("Enter number: ");
 		for (int i = 0; i <= n - 1; i++ ) {
-			println(n);
+			println("Hello");
 		}
 	}
 }
