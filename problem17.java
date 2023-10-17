337f8ca14d0dbecfb911a4b3da53445b49ee3887
S <s>
2023-10-17 16:13:34 +0400

    {"types":"C","runs":3}

--- problem17.java
+++ problem17.java
@@ -3,13 +3,9 @@ import acm.program.ConsoleProgram;
 public class problem17 extends ConsoleProgram {
 
 	public void run() {
-		double x = readInt("Enter your number: ");
-		if (x < 1) {
-			println("0 and x");
-		}
-		if (x > 1) {
-			for ( x > 1; x-- );
-			print(x);
-		}
-		}	
+		double x = readDouble("Enter your number: ");
+		println(x);
+		
+		
+	}
 }
