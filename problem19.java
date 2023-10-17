9a77211b724a806428d5537247e32a0ba40eebf8
S <s>
2023-10-17 16:32:29 +0400

    {"types":"A","runs":8}

---
+++ problem19.java
@@ -0,0 +1,18 @@
+import acm.program.ConsoleProgram;
+
+public class problem19 extends ConsoleProgram {
+
+	public void run() {
+	int x = readInt("Enter number: ");
+	int y = readInt("Enter second number: ");
+	if (x > y) {
+		print(x);
+	}
+	if (x < y) {
+		print(y);
+	}
+	if (x == y) {
+		println(x);
+	}
+	}
+}
