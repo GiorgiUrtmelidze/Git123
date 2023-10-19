87cf87fc9eb100497c7dd19a0bfafd2e9a6f821a
S <s>
2023-10-19 21:08:44 +0400

    {"types":"C","runs":0}

--- problem22.java
+++ problem22.java
@@ -1,22 +1,12 @@
 import acm.program.GraphicsProgram;
 
 public class problem22 extends GraphicsProgram {
-	private static final int NUM_LINES = 11;
-	public void run() {
-		for (int i = 0; i < NUM_LINES; i++) {
-			drawHorisontalLine(i);
-		}
-		
+	
 }
-	private void drawHorisontalLine(int i) {
-		double y1 = (getHeight()) / (NUM_LINES - 1);
-		double x1 = 0;
-		double x2 = getWidth();
-		GLine hLine = new GLine (x1, y1, x2, y2);
-		
+	public void run() {
+
 	
-	
-	
+	}
 	}
 	
 	
