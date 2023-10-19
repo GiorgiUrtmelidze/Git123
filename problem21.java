8b166fc2c3ee8930b8b18fdb42fd0323bfda232e
S <s>
2023-10-19 17:30:46 +0400

    {"types":"A","runs":0}

---
+++ problem21.java
@@ -0,0 +1,14 @@
+import acm.program.ConsoleProgram;
+
+public class problem21 extends ConsoleProgram {
+
+	public void run() {
+	int n = readInt("Enter: ");
+	int y = 0;
+	for (int i = 0; i < n; i++ ) {
+		int x = readInt("Enter number: ");
+		y = y + x;
+	}
+		print(y);
+			}
+}
