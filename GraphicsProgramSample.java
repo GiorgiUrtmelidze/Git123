3f3e5ab18a4f2d3ef241f60f4a585839eef7978d
S <s>
2023-10-24 16:38:08 +0400

    {"types":"C","runs":1}

--- GraphicsProgramSample.java
+++ GraphicsProgramSample.java
@@ -5,12 +5,24 @@
  * draws a partial diagram of the acm.graphics hierarchy.
  */
 
+import acm.graphics.*;
 import acm.program.*;
 
 public class GraphicsProgramSample extends GraphicsProgram {
 	
 	public void run() {
-		// You fill this in
+		double x1 = 200;
+		double y1 = 200;
+		GRect n1 = new GRect(x1,y1,50,50);
+		for (int i = 0; i < 5; i++) {
+			add(n1);
+			x1 = x1 + 80;
+			add(n1,x1);
+		}
+		
+		
+		
+	
 	}
 
 }
