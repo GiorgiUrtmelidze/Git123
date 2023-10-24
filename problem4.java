29113e564bc3e613c18776707a20a52a3eb9b82a
S <s>
2023-10-24 14:35:46 +0400

    {"types":"C","runs":12}

--- problem4.java
+++ problem4.java
@@ -4,11 +4,11 @@ public class problem4 extends ConsoleProgram {
 
 	public void run() {
 		println("This program calculates the hypotenuse with Pythagorean Theorem");
-		double a = readInt("Enter first number: ");
-		double b = readInt("Enter second number: ");
+		double a = readInt("Enter first number a: ");
+		double b = readInt("Enter second number b: ");
 		double cSquared = Math.pow(a, 2) + Math.pow(b, 2);
 		double c = Math.sqrt(cSquared);
-		println(c);
+		println("c = " + c +);
 		
 	}
 }
