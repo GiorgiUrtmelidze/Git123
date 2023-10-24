c2fe45ed9a34cf797b21bdfdd9be57e23329f369
S <s>
2023-10-24 16:15:51 +0400

    {"types":"C","runs":43}

--- problem1.java
+++ problem1.java
@@ -1,15 +1,9 @@
-import acm.program.ConsoleProgram;
+import acm.program.GraphicsProgram;
 
-public class problem1 extends ConsoleProgram {
+public class problem1 extends GraphicsProgram {
 
 	public void run() {
-		println("This program calculates the hypotenuse with Pythagorean Theorem");
-		double a = readInt("Enter first number a: ");
-		double b = readInt("Enter second number b: ");
-		double cSquared = Math.pow(a, 2) + Math.pow(b, 2);
-		double c = Math.sqrt(cSquared);
-		println("Hypotenuse c = " + c + "");
-		
+			
 	}
 }
 
