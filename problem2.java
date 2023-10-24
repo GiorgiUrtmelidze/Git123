f214f292958ef9c297c47f4147e90738802218f3
S <s>
2023-10-24 11:58:55 +0400

    {"types":"C","runs":2}

--- problem2.java
+++ problem2.java
@@ -5,12 +5,27 @@ import acm.program.GraphicsProgram;
 
 public class problem2 extends GraphicsProgram {
 	public void run() {
-		int x1 = getWidth() / 2 - 72;
-		int y1 = getHeight() / 2 - 72;
-		GOval firstCircle = new GOval (x1, y1, 144, 144);
+		
+//		FirstCircle Coordinates and position
+		double r1 = 72;
+		double x1 = getWidth() / 2 - r1;
+		double y1 = getHeight() / 2 - r1;
+		GOval firstCircle = new GOval (x1, y1, 2*r1, 2*r1);
 		firstCircle.setFilled (true);
 		firstCircle.setColor(Color.red);
 		add(firstCircle);
+		
+//		If 2.54cm = 72 pixel
+//		Then Z cm = (72*Z)/2.54 pixel
+		double r2 = (72 * 1.65) / 2.54;
+		double x2 = getWidth() / 2 - r2;
+		double y2 = getHeight() / 2 - r2;
+		GOval secondCircle = new GOval (x2, y2, 2*r2, 2*r2);
+		secondCircle.setFilled (true);
+		secondCircle.setColor(Color.white);
+		add(secondCircle);
+		
+		
 	  
 	}
 }
