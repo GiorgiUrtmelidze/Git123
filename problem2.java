959bb333c3bec07751167d26e1c4a0bc52ca6794
S <s>
2023-10-24 11:21:34 +0400

    {"types":"A","runs":0}

---
+++ problem2.java
@@ -0,0 +1,24 @@
+import acm.program.ConsoleProgram;
+
+public class problem2 extends ConsoleProgram {
+
+	public void run() {
+		println("Hello");
+		int numberOne = readInt("Enter First Number: ");
+		int numberSecond = readInt("Enter Second Number: ");
+		int total = ((numberOne + numberSecond)/2 );
+		double y = total + 0.5;
+		if ((numberOne + numberSecond)%2==0) {
+		println("The answer is ->" + total + "."); 
+		} else {
+			println("The answer is ->" + y + ".");
+		}
+		
+	
+	}
+
+
+
+	
+	
+}
