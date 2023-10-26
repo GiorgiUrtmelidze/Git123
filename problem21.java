866a04473f169c1c93685e868c5cf64a97d20df9
S <s>
2023-10-26 17:41:21 +0400

    {"types":"C","runs":3}

--- problem21.java
+++ problem21.java
@@ -6,16 +6,24 @@ public class problem21 extends GraphicsProgram {
 	public void run() {
 		for (int i = 0; i < NUM_LINES; i++) {
 			drawHorisontalLine(i);
+			drawVerticalLine(i);
 		}
 		
 		
 }
+	private void drawVerticalLine(int i) {
+		double x3 = i*(getWidth() / (NUM_LINES - 1));
+		double y3 = 0;
+		double y4 = getHeight();
+		GLine vLine = new GLine (x3, y4, x3, y4);
+		add(vLine);
+		
+	}
 	private void drawHorisontalLine(int i) {
 		double y1 = (getHeight()) / (NUM_LINES - 1)*i;
 		double x1 = 0;
 		double x2 = getWidth();
-		double y2 = y1;
-		GLine hLine = new GLine (x1, y1, x2, y2);
+		GLine hLine = new GLine (x1, y1, x2, y1);
 		add(hLine);
 	}
 }
