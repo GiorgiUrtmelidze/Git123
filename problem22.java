24cba143833e472d545ba38205be4dcbe77d66c8
S <s>
2023-10-26 18:01:30 +0400

    {"types":"C","runs":4}

--- problem22.java
+++ problem22.java
@@ -1,10 +1,19 @@
+import acm.graphics.GRect;
 import acm.program.GraphicsProgram;
 
 public class problem22 extends GraphicsProgram {
-//	n^m
-
-	public void run() {
-		
+	private static final int NUM_SQUARES = 8;
+		public void run() {
+			double sqSize = getHeight() / NUM_SQUARES;
+		for (int i = 0; i < NUM_SQUARES; i++) {
+			for (int j = 0; j < NUM_SQUARES; j++) {
+				double x = j*sqSize;
+				double y = i*sqSize;
+				GRect square = new GRect(x, y, sqSize, sqSize);
+				add(square);
+				if (j % 2 == 0 )
+			}
+		}
 	
 	}
 }
