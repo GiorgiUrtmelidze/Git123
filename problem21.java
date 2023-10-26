1fcb1ad5d8cdc3d19c17e3ff1b728def983da119
S <s>
2023-10-26 17:34:26 +0400

    {"types":"C","runs":1}

--- problem21.java
+++ problem21.java
@@ -1,3 +1,4 @@
+import acm.graphics.GLine;
 import acm.program.GraphicsProgram;
 
 public class problem21 extends GraphicsProgram {
@@ -7,17 +8,16 @@ public class problem21 extends GraphicsProgram {
 			drawHorisontalLine(i);
 		}
 		
+		
 }
 	private void drawHorisontalLine(int i) {
-		double y1 = (getHeight()) / (NUM_LINES - 1);
+		double y1 = (getHeight()) / (NUM_LINES - 1)*i;
 		double x1 = 0;
 		double x2 = getWidth();
+		double y2 = y1;
 		GLine hLine = new GLine (x1, y1, x2, y2);
-		
-	
-	
-	
 	}
+}
 	
 	
 	
