2ea44b171006c435ea7ad7fd2bacc8e54f0db027
S <s>
2023-10-28 16:24:24 +0400

    {"types":"C","runs":10}

--- problem23.java
+++ problem23.java
@@ -3,27 +3,16 @@ import acm.program.ConsoleProgram;
 public class problem23 extends ConsoleProgram {
 
 	public void run() {
-	int n = readInt("Enter n: ");
-	int sum = calculateSum(n);
-	println("sum of numbers 1...n is " + sum);
-	int numDivisors = numberOfDivisors(n);
-}
-	
-
-	private int calculateSum(int n) {
-		int sum = 0;
-		for(int i = 1; i <= n; i++) {
-			sum += i; //sum = sum + i
-		}
-		return sum;
+		int n = readInt("Enter number: ");
+		int sum = calculateN(n);
+		print(sum);
 	}
-	private int numberOfDivisors(int n) {
-		int num = 0;
-		for(int i = 1; i <= n; i++) {
-			if ( n % i == 0 ) {
-				num += 1;
-			}
-			return num;
+
+	private int calculateN(int n) {
+		int sum = 0;
+		for(int i = 0; i < n; i++) {
+			sum += i; 
 		}
+		return (sum);
 	}
 }
