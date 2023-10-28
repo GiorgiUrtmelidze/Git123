2a82b3af6a38224281bfb0da00e03f0ac2fa1f63
S <s>
2023-10-28 16:56:06 +0400

    {"types":"AC","runs":26}

---
+++ problem27.java
@@ -0,0 +1,22 @@
+import acm.program.ConsoleProgram;
+
+public class problem27 extends ConsoleProgram {
+
+	public void run() {
+		int n1 = readInt("Enter first number: ");
+		int n2 = readInt("Enter second number: ");
+		int gcm = calculategcm(n1, n2);
+		println(gcm);
+		
+	}
+
+	private int calculategcm(int n1, int n2) {
+		int gcm = 0;
+		for (gcm = n1; ;gcm--) {
+			if (n1%gcm == 0 && n2%gcm == 0) {
+				return gcm;
+			}
+		}
+		
+	}
+}
