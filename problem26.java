f9fd9e839be9fd368d16422f652474c47a4b027a
S <s>
2023-10-28 16:55:35 +0400

    {"types":"C","runs":26}

--- problem26.java
+++ problem26.java
@@ -5,15 +5,16 @@ public class problem26 extends ConsoleProgram {
 	public void run() {
 		int n1 = readInt("Enter first number: ");
 		int n2 = readInt("Enter second number: ");
-		int lcm = calculatelcm(n1, n2);
-		println(lcm);
+		int gcm = calculategcm(n1, n2);
+		println(gcm);
+		
 	}
 
-	private int calculatelcm(int n1, int n2) {
-		int lcm = 0;
-		for (lcm = n1; ;lcm++) {
-			if (lcm%n1 == 0 && lcm%n2 == 0) {
-			return lcm;
+	private int calculategcm(int n1, int n2) {
+		int gcm = 0;
+		for (gcm = n1; ;gcm--) {
+			if (n1%gcm == 0 && n2%gcm == 0) {
+				return gcm;
 			}
 		}
 		
