1c55da3a0d341a6d96aa5b219490c89ba906d817
S <s>
2023-10-28 16:49:54 +0400

    {"types":"C","runs":23}

--- problem25III.java
+++ problem25III.java
@@ -3,20 +3,19 @@ import acm.program.ConsoleProgram;
 public class problem25III extends ConsoleProgram {
 
 	public void run() {
-		int n = readInt("Enter number: ");
-		int numberOfDivisors = calculation(n);
-		println(numberOfDivisors);
+		int n1 = readInt("Enter first number: ");
+		int n2 = readInt("Enter second number: ");
+		int lcm = calculatelcm(n1, n2);
+		println(lcm);
 	}
 
-	private int calculation(int n) {
-		int numberOfDivisors = 0;
-		for (int i = 1; i <= n; i++) {
-			if (n%i == 0) {
-				numberOfDivisors += 1;
+	private int calculatelcm(int n1, int n2) {
+		int lcm = 0;
+		for (lcm = n1; ;lcm++) {
+			if (lcm%n1 == 0 && lcm%n2 == 0) {
+			return lcm;
 			}
 		}
 		
-		
-		return numberOfDivisors;
 	}
 }
