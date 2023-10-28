4b6fe2d26d82d8b40f854c2b8ce9d86339b64ce5
S <s>
2023-10-28 16:38:00 +0400

    {"types":"A","runs":22}

---
+++ problem25III.java
@@ -0,0 +1,22 @@
+import acm.program.ConsoleProgram;
+
+public class problem25III extends ConsoleProgram {
+
+	public void run() {
+		int n = readInt("Enter number: ");
+		int numberOfDivisors = calculation(n);
+		println(numberOfDivisors);
+	}
+
+	private int calculation(int n) {
+		int numberOfDivisors = 0;
+		for (int i = 1; i <= n; i++) {
+			if (n%i == 0) {
+				numberOfDivisors += 1;
+			}
+		}
+		
+		
+		return numberOfDivisors;
+	}
+}
