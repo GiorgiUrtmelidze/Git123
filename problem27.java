eaf18e48dc6e78d128c569191474ed598c527893
S <s>
2023-10-28 17:10:02 +0400

    {"types":"C","runs":35}

--- problem27.java
+++ problem27.java
@@ -3,10 +3,11 @@ import acm.program.ConsoleProgram;
 public class problem27 extends ConsoleProgram {
 
 	public void run() {
+		println("This program only calculates n^m only for positive integers");
 		int n = readInt("Enter number: ");
 		int m = readInt("Enter second number: ");
-		if (n == 0 && m == 0) {
-			println("This does not exist");
+		if (n <= 0 || m <= 0) {
+			println("This program only calculates n^m only for positive integers");
 		} else {
 		int nToThePowerM = calculate(n, m);
 		println(nToThePowerM);
