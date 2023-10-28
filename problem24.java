6dbd1b78aeee686df7cc830956019aff1557d2de
S <s>
2023-10-28 16:32:46 +0400

    {"types":"C","runs":12}

--- problem24.java
+++ problem24.java
@@ -3,6 +3,19 @@ import acm.program.ConsoleProgram;
 public class problem24 extends ConsoleProgram {
 
 	public void run() {
-}
+		int n = readInt("Enter number: ");
+		int numberOfDivisors = calculation(n);
+	}
+
+	private int calculation(int n) {
+		int numberOfDivisors = 0;
+		for (int i = 1; i < n; i++) {
+			if (n%i == 0) {
+				numberOfDivisors += i;
+			}
+		}
+		
+		
+		return calculation(n);
 	}
 }
