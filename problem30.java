ec3cd3830d5e247cbd22b95fee50071c7376f628
S <s>
2023-10-28 19:02:05 +0400

    {"types":"C","runs":58}

--- problem30.java
+++ problem30.java
@@ -4,9 +4,9 @@ public class problem30 extends ConsoleProgram {
 
 	public void run() {
 		int n = readInt("Enter number: ");
-		for(int i = 10; ; i= i*10) {
+		for (int i = 10;  ;i*=10) {
 			for (int x = 0; ; x++) {
-				if (x%10 == 0 && n%x < i) {
+				if (x%10 == 0 && n%x < i) { 
 					println(n-x);
 				}
 			}
