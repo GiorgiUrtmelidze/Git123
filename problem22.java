690f0f06b9800af513327bb266f53ce220a90569
S <s>
2023-10-28 16:15:41 +0400

    {"types":"C","runs":6}

--- problem22.java
+++ problem22.java
@@ -4,24 +4,31 @@ import acm.program.GraphicsProgram;
 public class problem22 extends GraphicsProgram {
 	private static final int NUM_SQUARES = 27;
 		public void run() {
+			colorChessBoard();
+		}
+			
+		private void colorChessBoard() {
 			double sqSize = getHeight() / NUM_SQUARES;
-		for (int i = 0; i < NUM_SQUARES; i++) {
-			for (int j = 0; j < NUM_SQUARES; j++) {
-				double x = j*sqSize;
-				double y = i*sqSize;
-				GRect square = new GRect(x, y, sqSize, sqSize);
-				add(square);
-				if (i%2 == 0 && j%2 != 0) {
-				square.setFilled(true);
-				}
-				if (i%2 != 0 && j%2 == 0) {
-				square.setFilled(true);
+			for (int i = 0; i < NUM_SQUARES; i++) {
+				for (int j = 0; j < NUM_SQUARES; j++) {
+					double x = j*sqSize;
+					double y = i*sqSize;
+					GRect square = new GRect(x, y, sqSize, sqSize);
+					add(square);
+					
+					if (i%2 == 0 && j%2 != 0) {
+					square.setFilled(true);
+					}
+					if (i%2 != 0 && j%2 == 0) {
+					square.setFilled(true);
+					}
 				}
 			}
-		}
-	
+			
 	}
+	
 }
+
 	
 	
 	
