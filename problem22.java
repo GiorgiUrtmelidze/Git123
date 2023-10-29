9e476edc4fba982b6abdbcc78ccec335d005b086
S <s>
2023-10-29 20:55:53 +0400

    {"types":"C","runs":85}

--- problem22.java
+++ problem22.java
@@ -2,7 +2,7 @@ import acm.graphics.GRect;
 import acm.program.GraphicsProgram;
 
 public class problem22 extends GraphicsProgram {
-	private static final int NUM_SQUARES = 50;
+	private static final int NUM_SQUARES = 100;
 		public void run() {
 			colorChessBoard();
 		}
